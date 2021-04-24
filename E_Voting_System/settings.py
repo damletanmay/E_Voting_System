040abf9c2121196c618b856cbc904a57b4c59056
damletanmay <tanmay.damle.25@gmail.com>
2021-04-24 19:47:29 +0530

    test 2

--- E_Voting_System/settings.py
+++ E_Voting_System/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = 'asj_b8huty-4jgmv11&%jd0@ni1&)6pe4z45ykokxpm)6q1fda'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost','127.0.0.1','e-voting--system.herokuapp.com']
 
